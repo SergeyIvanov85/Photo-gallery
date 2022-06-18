b4d7c2bea22cd911249566b74d5893b34d63591e
Sergei Ivanov <koldyn85@yandex.ru>
2022-06-18 11:39:44 +0300

    feat: add mansory lib

--- scripts/createCardPhoto.js
+++ scripts/createCardPhoto.js
@@ -1,34 +1,45 @@
-import { createElem } from "./createElem.js";
+import { createElem } from './createElem.js';
 
-export const createCardPhoto = (data) => {
-  const card = createElem('li', {
-    className: 'card'
+const loadImg = (url, description) => {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.width = 200;
+    img.src = url;
+    img.alt = description;
+    img.addEventListener('load', () => {
+      resolve(img);
+    });
+    img.addEventListener('error', (err) => {
+      reject(new Error(err));
+    });
   });
+};
+
+export const createCardPhoto = async (data) => {
   
+  const card = createElem('li', {
+    className: 'card',
+  });
 
   const cardItem = createElem('a', {
     id: data.id,
     className: 'grid-item',
     href: `page.html?photo=${data.id}`,
   });
- 
-  const photo = new Image();
-  photo.width = '200';
-  photo.src = data.urls.small;
-  photo.alt = data.alt_description;
+
+  const photo = await loadImg(data.urls.small, data.alt_description);
 
   const author = createElem('a', {
     className: 'card__author',
     href: data.user.links.html,
   });
 
-
   const avatarAuthor = new Image();
   avatarAuthor.className = 'author__photo';
-  avatarAuthor.src = data.user.profile_image.medium; 
+  avatarAuthor.src = data.user.profile_image.medium;
   avatarAuthor.width = '32';
-  avatarAuthor.height = '32'; 
-  avatarAuthor.alt = data.user.bio; 
+  avatarAuthor.height = '32';
+  avatarAuthor.alt = data.user.bio;
   avatarAuthor.title = data.user.username;
 
   author.append(avatarAuthor);
@@ -38,7 +49,6 @@ export const createCardPhoto = (data) => {
     textContent: data.likes,
   });
 
-
   const downloadLink = createElem('a', {
     className: 'card__download',
     href: data.links.download,
@@ -47,6 +57,7 @@ export const createCardPhoto = (data) => {
   });
 
   cardItem.append(photo, author, likeBtn, downloadLink);
+  
   card.append(cardItem);
 
   return card;
--- scripts/renderGallery.js
+++ scripts/renderGallery.js
@@ -1,9 +1,24 @@
-import { createCardPhoto } from "./createCardPhoto.js";
+import { createCardPhoto } from './createCardPhoto.js';
+import { createElem } from './createElem.js';
 
+export const renderGallery = (wrapper, photos) => {
+  const gallery = createElem('ul', {
+    className: 'grid',
+  });
+  
+  wrapper.append(gallery);
 
+  const grid = new Masonry(gallery, {
+    gutter: 10,
+    itemSelector: '.card',
+    columnWidth: 200,
+    isFitWidth: true,
+  });
 
-export const renderGallery = (photos) => {
-  const gallery = document.querySelector('.grid');
   const cards = photos.map(createCardPhoto);
-  gallery.append(...cards)
-}
+
+  Promise.all(cards).then((cards) => {
+    gallery.append(...cards);
+    grid.appended(cards);
+  });
+};
